1921ca81ac5fe8e5d8c751b68c5e4a841b0d64d9
Angela Bosilkovska <angelabosilkovska99@gmail.com>
2024-02-06 09:57:31 +0100

    new model customer, project clean up

--- src/main/java/com/myfirstproject/controller/Controller.java
+++ src/main/java/com/myfirstproject/controller/Controller.java
@@ -1,21 +1,14 @@
-package com.myfirstproject;
+package com.myfirstproject.controller;
 
 import com.myfirstproject.dto.GreetResponse;
 import com.myfirstproject.dto.Person;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@SpringBootApplication
 @RestController
-public class Main {
-
-    public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
-    }
+public class Controller {
 
     @GetMapping("/greet")
     public GreetResponse greet() {
